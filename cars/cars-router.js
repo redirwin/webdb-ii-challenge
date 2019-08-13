0e6be37acd2f9c67328328a638b1a04cdaf1881f
Dave Irwin <david@redirwin.com>
2019-08-13 15:28:46 -0700

    Installed knex and sqlite3. Created migration file.

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const knex = require("knex");
 
+const config = require("../knexfile.js");
+
+const db = knex(config.development);
+
 const router = express.Router();
 
 // endpoints
